93175dd058390811e78357f3ddd42ee4a3d2ad23
ssalamanca <santiago.anime@hotmail.com>
2016-11-20 17:12:16 -0500

    Añado CrearLiderGrupo

--- SplitPay/src/Conecciones/ConeccionDatos.java
+++ SplitPay/src/Conecciones/ConeccionDatos.java
@@ -11,65 +11,67 @@ import java.io.FileNotFoundException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 /**
  *
  * @author santi
  */
 public class ConeccionDatos implements ConeccionBases {
+     String username = "is102317";
+    String password = "bQmLIqN6HV";
+String thinConn = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR";
 
-    public void CrearUsuario() {
-        String username = "is102317";
-        String password = "bQmLIqN6HV";
-        String thinConn = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR";
+    public void CrearUsuario(String user_name,String nombre,int telefono,String email,String paypal,String apellido,Date fecha, String genero,String contrasena) {
+
         try {
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102311", "DAbTstfDxY");
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
             CallableStatement cSmt = conn.prepareCall("{call RegistrarUsuario (?,?,?,?,?,?,?,?,?,?)");
-            cSmt.setString(1, "santiash");
+            cSmt.setString(1, user_name);
             // Revisar si el username nuevo que ingresan ya existe en la base de datos
-            cSmt.setString(2, "Santiago");
-            cSmt.setInt(3, 32065426);
-            cSmt.setString(4, "snjsksam@gmail.com");
-            cSmt.setString(5, "Cuenta_paypal");
-            cSmt.setString(6, "salamanca");
-            int anio = 1997, mes = 05, dia = 23;
-            Date fecha = new Date(anio, mes, dia);
+            cSmt.setString(2, nombre);
+            cSmt.setInt(3, telefono);
+            cSmt.setString(4, email);
+            cSmt.setString(5, paypal);
+            cSmt.setString(6, apellido);
+           // int anio = 1997, mes = 05, dia = 23;
+           // Date fecha = new Date(anio, mes, dia);
             cSmt.setDate(7, fecha);
-            cSmt.setString(8, "M");
-            cSmt.setString(9, "xdabcd");
+            cSmt.setString(8, genero);
+            cSmt.setString(9, contrasena);
             cSmt.setString(10, "N");
             cSmt.execute();
-            System.out.println("Cambio el dato");
         } catch (SQLException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
     }
 
-    public void CrearGrupo() {
+    public void CrearGrupo(String nombre,int duenoID) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "DAbTstfDxY");
-            CallableStatement cSmt = conn.prepareCall("{call CrearGrupo (?,?,?)");
-            cSmt.setString(1, "grupo_10");
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
+            CallableStatement cs = conn.prepareCall("{call CrearGrupo (?,?,?)");
+            cs.setString(1, nombre);
             // Revisar si el nombre del grupo nuevo que ingresan ya existe en la base de datos
-            cSmt.setInt(2, 21);
+            cs.setInt(2, duenoID);
             Calendar fecha = new GregorianCalendar();
             int anio = fecha.get(Calendar.YEAR);
             int mes = fecha.get(Calendar.MONTH);
             int dia = fecha.get(Calendar.DAY_OF_MONTH);
             Date fechaDate = new Date(anio, mes, dia);
-            cSmt.setDate(3, fechaDate);
-            cSmt.execute();
-            System.out.println("Cambio el dato");
+            cs.setDate(3, fechaDate);
+            cs.execute();
         } catch (SQLException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
     }
-    public void agregarRecibo(String ubi,String nombre,String comentarios,int costo,int idUser,int IdGrupo) throws SQLException, FileNotFoundException{
+     public void agregarRecibo(String ubi,String nombre,String comentarios,int costo,int idUser,int IdGrupo) throws SQLException, FileNotFoundException{
     	
     	Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102311", "bQmLIqN6HV");
     	File ubicacion = new File(ubi);
@@ -82,5 +84,20 @@ public class ConeccionDatos implements ConeccionBases {
     	//ps.setBinaryStream(1, archivo,ubicacion.length());
 
     	ps.execute();
+}
+    public void CrearLiderGrupo(int idGrupo, int idnuevoLider,Date dateIngreso){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
+            CallableStatement cs = conn.prepareCall("{call CrearLiderGrupo (?,?,?)");
+            cs.setDate(1,dateIngreso) ;
+            cs.setInt(2, idGrupo);
+            cs.setInt(3, idnuevoLider);
+            cs.execute();
+            System.out.println("Cambio el dato");
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        
     }
+    
 }
