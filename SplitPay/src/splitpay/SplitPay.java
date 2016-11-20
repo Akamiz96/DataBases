7125d159be441ba7bda302c6d1d6e4ce2b77c97d
pabloarizaluna <pablo_ariza@javeriana.edu.co>
2016-11-20 09:09:59 -0500

    transaccion miembro a miembro

--- SplitPay/src/splitpay/SplitPay.java
+++ SplitPay/src/splitpay/SplitPay.java
@@ -6,7 +6,12 @@
 package splitpay;
 
 import Controladores.CuentaJpaController;
+import Controladores.DeudaJpaController;
+import Controladores.TransaccionJpaController;
 import Controladores.UsuarioJpaController;
+import Negocio.Deuda;
+import Negocio.DeudaPK;
+import Negocio.TransaccionPK;
 import Negocio.Usuario;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
@@ -38,4 +43,17 @@ public class SplitPay {
      /*  controCuenta.RealizarBalanceGruposdeUsuario();*/
     }
     
+    
+    public static void memberToMemberTrans(int idPK, long deudaCuentaId, int deudaUsuarioId, short deudaIdDeuda, EntityManagerFactory emf, long cantidad, char tipo)
+    {
+        TransaccionJpaController controTrans = new TransaccionJpaController(emf);
+        DeudaJpaController controDeuda = new DeudaJpaController(emf);
+        TransaccionPK referencias = new TransaccionPK(idPK, deudaCuentaId, deudaUsuarioId, deudaIdDeuda);
+        Deuda deuda = controDeuda.findDeuda(new DeudaPK());
+        controTrans.memberToMemberTrans(referencias, cantidad, tipo);
+        if(deuda.getCantidad() - cantidad < 0)
+        {
+            //TODO crear una deuda si la transaccion es mayor a la deuda a la persona que se le hizo la transaccion
+        }
+    }
 }
