2a5863531a01ee16f938b602dbe4fc2ad9d08511
andrescontreras <andres260926@hotmail.com>
2016-11-20 16:05:59 -0500

    agregar cuenta

--- SplitPay/src/splitpay/SplitPay.java
+++ SplitPay/src/splitpay/SplitPay.java
@@ -1,19 +1,13 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package splitpay;
 
 import Controladores.CuentaJpaController;
-import Controladores.DeudaJpaController;
-import Controladores.TransaccionJpaController;
+import Controladores.GrupoJpaController;
 import Controladores.UsuarioJpaController;
-import Negocio.Deuda;
-import Negocio.DeudaPK;
-import Negocio.TransaccionPK;
+import Negocio.Grupo;
+
 import java.util.List;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -28,24 +22,10 @@ public class SplitPay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SplitPayPU");
-        UsuarioJpaController contro = new UsuarioJpaController(emf) ;
-        //contro.signIn( "hola","12345" );     
-        DeudaPK deuda;
-        deuda = new DeudaPK(2, (long)1, (short)1);
-        
-        
-        TransaccionJpaController controTrans = new TransaccionJpaController(emf);
-        System.out.println( controTrans.memberToMemberTrans( deuda.getUsuarioId(), deuda.getCuentaId(), deuda.getIdDeuda(), 50000, 'C') );
-        
-        
-        System.out.println(" Hola Mundo") ;
-        int idGrupo = 1 ;
-        int idUsu= 1 ;
-        CuentaJpaController controCuenta = new CuentaJpaController(emf);
-       // controCuenta.GruposdeUsuario(idUsu);
-       // List<String>usu=contro.UsuariosdeGrupo(idGrupo);
-     /*  controCuenta.RealizarBalanceGruposdeUsuario();*/
-    }
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("SplitPayPU");
+		GrupoJpaController gr = new GrupoJpaController(emf);
+	
+		
+    
 }
-
+}
