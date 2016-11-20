dfc08260606c0f9df20da3e175a72b1040bd1c48
ssalamanca <santiago.anime@hotmail.com>
2016-11-20 14:30:59 -0500

    Agregue metodo para crear LiderGrupo

--- SplitPay/src/Conecciones/ConeccionDatos.java
+++ SplitPay/src/Conecciones/ConeccionDatos.java
@@ -13,20 +13,23 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 /**
  *
  * @author santi
  */
 public class ConeccionDatos implements ConeccionBases {
-    String username = "is102317";
+     String username = "is102317";
     String password = "bQmLIqN6HV";
-    String thinConn = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR";
-    
-    @Override
+String thinConn = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR";
+
     public void CrearUsuario() {
+
         try {
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102311", "DAbTstfDxY");
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
             CallableStatement cSmt = conn.prepareCall("{call RegistrarUsuario (?,?,?,?,?,?,?,?,?,?)");
             cSmt.setString(1, "santiash");
             // Revisar si el username nuevo que ingresan ya existe en la base de datos
@@ -48,21 +51,20 @@ public class ConeccionDatos implements ConeccionBases {
         }
     }
 
-    @Override
     public void CrearGrupo() {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102311", "DAbTstfDxY");
-            CallableStatement cSmt = conn.prepareCall("{call CrearGrupo (?,?,?)");
-            cSmt.setString(1, "grupo_10");
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
+            CallableStatement cs = conn.prepareCall("{call CrearGrupo (?,?,?)");
+            cs.setString(1, "grupo_10");
             // Revisar si el nombre del grupo nuevo que ingresan ya existe en la base de datos
-            cSmt.setInt(2, 21);
+            cs.setInt(2, 21);
             Calendar fecha = new GregorianCalendar();
             int anio = fecha.get(Calendar.YEAR);
             int mes = fecha.get(Calendar.MONTH);
             int dia = fecha.get(Calendar.DAY_OF_MONTH);
             Date fechaDate = new Date(anio, mes, dia);
-            cSmt.setDate(3, fechaDate);
-            cSmt.execute();
+            cs.setDate(3, fechaDate);
+            cs.execute();
             System.out.println("Cambio el dato");
         } catch (SQLException ex) {
             System.out.println("Error: " + ex.getMessage());
@@ -71,7 +73,7 @@ public class ConeccionDatos implements ConeccionBases {
     
     public void crearTransaccion( int idPK, int usuarioId, long cuentaId, short idDeuda, long cantidad, String tipo) throws SQLException
     {
-        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102311", "DAbTstfDxY");
+        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
         PreparedStatement insertar = conn.prepareStatement("INSERT INTO transaccion(id,FECHA,CANTIDAD,DEUDA_CUENTA_ID,DEUDA_USUARIO_ID,DEUDA_ID_DEUDA,TIPO) values(?,CURRENT_DATE,?,?,?,?,?);");
         insertar.setInt(1, idPK);
         insertar.setLong(2, cantidad);
@@ -80,4 +82,20 @@ public class ConeccionDatos implements ConeccionBases {
         insertar.setShort(5, idDeuda);
         insertar.setString(6, tipo);
     }
+   
+    public void CrearLiderGrupo(int idGrupo, int idnuevoLider,Date dateIngreso){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDISOR", "is102317", "bQmLIqN6HV");
+            CallableStatement cs = conn.prepareCall("{call CrearLiderGrupo (?,?,?)");
+            cs.setDate(1,dateIngreso) ;
+            cs.setInt(2, idGrupo);
+            cs.setInt(3, idnuevoLider);
+            cs.execute();
+            System.out.println("Cambio el dato");
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        
+    }
+    
 }
