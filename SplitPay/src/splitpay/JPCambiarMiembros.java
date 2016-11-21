4e9eee45c608d2cbee2292bf3d652defca259e49
kstro96 <alejandrocastro9b@hotmail.com>
2016-11-20 19:40:00 -0500

    Add files via upload

--- SplitPay/src/splitpay/JPCambiarMiembros.java
+++ SplitPay/src/splitpay/JPCambiarMiembros.java
@@ -2,12 +2,16 @@ package splitpay;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Arrays;
+import java.util.Vector;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class JPCambiarMiembros extends JPanel {
 
@@ -17,7 +21,12 @@ public class JPCambiarMiembros extends JPanel {
 	private GUIPrincipal principal;
 	private PMenu menu;
 	private JTable table;
-	private JTable table_1;
+	private String[] columnSer = { "Nombre de Usuario" };
+	private Vector rowDataSer;
+	private Vector columSerV;
+	private JScrollPane scrollPane;
+	private JScrollPane scrollPane_1;
+	private JTable table_2;
 	public JPCambiarMiembros(GUIPrincipal principal, PMenu menu) {
 		this.principal = principal;
 		this.menu = menu;
@@ -36,25 +45,35 @@ public class JPCambiarMiembros extends JPanel {
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 36));
 		panel.add(lblNewLabel);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setBounds(100, 130, 235, 384);
 		panel.add(scrollPane);
 		
-		table = new JTable();
+		table = getTable();
 		scrollPane.setViewportView(table);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(515, 130, 235, 384);
 		panel.add(scrollPane_1);
 		
-		table_1 = new JTable();
-		scrollPane_1.setViewportView(table_1);
+		table_2 = new JTable();
+		scrollPane_1.setViewportView(table_2);
 		
 		JButton btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO ADICIONAR UN MIEMBRO
+			}
+		});
 		btnAdicionar.setBounds(378, 228, 89, 23);
 		panel.add(btnAdicionar);
 		
 		JButton btnRetirar = new JButton("Retirar");
+		btnRetirar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO RETIRAR UN MIEMBRO 
+			}
+		});
 		btnRetirar.setBounds(378, 297, 89, 23);
 		panel.add(btnRetirar);
 		
@@ -65,5 +84,31 @@ public class JPCambiarMiembros extends JPanel {
 		JLabel lblContactos = new JLabel("Contactos");
 		lblContactos.setBounds(532, 77, 192, 33);
 		panel.add(lblContactos);
+	}
+	private JTable getTable() {
+		if (table == null) {
+			columSerV = new Vector(Arrays.asList(this.columnSer));
+			rowDataSer = new Vector();
+			table = new JTable(this.rowDataSer, columSerV);
+			scrollPane.setViewportView(table);
+
+		}
+		return table;
+	}
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
+	public JScrollPane getScrollPane_1() {
+		return scrollPane_1;
+	}
+	public JTable getTable_2() {
+		if (table_2 == null) {
+			columSerV = new Vector(Arrays.asList(this.columnSer));
+			rowDataSer = new Vector();
+			table_2 = new JTable(this.rowDataSer, columSerV);
+			scrollPane.setViewportView(table_2);
+
+		}
+		return table_2;
 	}
 }
